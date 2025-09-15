bfe78dd341aa8391a1003355cd97f21e229c8bec
lilianevale <lili_malman@yahoo.com.br>
2025-09-15 03:11:01 -0300

    Update sapatas.py

--- app/routes/sapatas.py
+++ app/routes/sapatas.py
@@ -5,7 +5,7 @@ import subprocess
 import sys
 
 from itertools import combinations
-from app.utils.calculos import carregar_dados, estaca_info, capacidade_carga, download_excel, plot_data, save_dxf, vazio
+from app.utils.calculos import  download_excel, plot_data, vazio
 
 sapatas = Blueprint('sapatas', __name__)
 
